54063f744937d9da45604071aaf89f10b37d9103
Nico <nico.huynh@gmail.com>
2018-09-15 13:31:37 +0200

    fixing slice

--- srcs/thread.c
+++ srcs/thread.c
@@ -13,12 +13,9 @@ void		mt_init(t_env *env)
     i = -1;
     while(++i < THREAD_COUNT)
     {
-		env->s[i].y1 = env->d.y1 + i * env->d.step * PXL_HEIGHT;
-		env->s[i].y2 = env->d.y1 + (i + 1) * env->d.step * PXL_HEIGHT;
-        printf("\nSlice %i btw %f / %f.", i, env->s[i].y1, env->s[i].y2);
-        ft_putchar('+');
+		env->s[i].y1 = env->d.y1 + i * (env->d.y2 - env->d.y1) / THREAD_COUNT;
+		env->s[i].y2 = env->d.y1 + (i + 1) * (env->d.y2 - env->d.y1) / THREAD_COUNT;
         if (!(env->s[i].data = ft_memalloc(sizeof(t_pxl) * SLICE_LEN)))
             quit_program(env, EXIT_FAILURE);
     }
-    printf("the i is %i", i);
 }
