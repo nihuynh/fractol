94e63098c0eecd4bf2e21d4065baf6e50122433f
Nicolas HUYNH <nihuynh@student.42.fr>
2018-09-27 15:02:18 +0200

    small tweaks

--- srcs/hud.c
+++ srcs/hud.c
@@ -6,7 +6,7 @@
 /*   By: nihuynh <nihuynh@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/09/15 15:29:23 by nihuynh           #+#    #+#             */
-/*   Updated: 2018/09/26 01:05:55 by nihuynh          ###   ########.fr       */
+/*   Updated: 2018/09/27 14:48:38 by nihuynh          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,8 +21,8 @@ static inline void	hud_mouse(t_env *env)
 
 	if (!(val = ft_strdup((env->motion_on == 1) ? "on" : "off")))
 		return ;
-	mlx_string_put(env->mlx, env->win, 15, 10, 16777215, "Mouse : ");
-	mlx_string_put(env->mlx, env->win, 95, 10, 16777215, val);
+	mlx_string_put(env->mlx, env->win, 15, 10, C_UI, "Mouse : ");
+	mlx_string_put(env->mlx, env->win, 95, 10, C_UI, val);
 	free(val);
 }
 
@@ -36,8 +36,8 @@ static inline void	hud_value(t_env *env, int value, char *str, int pos)
 	xv = 15 + ft_strlen(str) * 10;
 	if (!(val = ft_itoa(value)))
 		return ;
-	mlx_string_put(env->mlx, env->win, 15, yv, 16777215, str);
-	mlx_string_put(env->mlx, env->win, xv, yv, 16777215, val);
+	mlx_string_put(env->mlx, env->win, 15, yv, C_UI, str);
+	mlx_string_put(env->mlx, env->win, xv, yv, C_UI, val);
 	free(val);
 }
 
@@ -51,8 +51,8 @@ static inline void	hud_dvalue(t_env *env, double value, char *str, int pos)
 	xv = 15 + ft_strlen(str) * 10;
 	if (!(val = ft_dtoa(value, DTOA_DSCP)))
 		return ;
-	mlx_string_put(env->mlx, env->win, 15, yv, 16777215, str);
-	mlx_string_put(env->mlx, env->win, xv, yv, 16777215, val);
+	mlx_string_put(env->mlx, env->win, 15, yv, C_UI, str);
+	mlx_string_put(env->mlx, env->win, xv, yv, C_UI, val);
 	free(val);
 }
 
@@ -63,13 +63,13 @@ static inline void	hud_dvalue(t_env *env, double value, char *str, int pos)
 static inline void	hud_help(t_env *env)
 {
 	env->hud_cmd--;
-	mlx_string_put(env->mlx, env->win, VP_WIDTH - 350, 10, 16777215, CMD_1);
-	mlx_string_put(env->mlx, env->win, VP_WIDTH - 350, 30, 16777215, CMD_2);
-	mlx_string_put(env->mlx, env->win, VP_WIDTH - 350, 50, 16777215, CMD_3);
-	mlx_string_put(env->mlx, env->win, VP_WIDTH - 350, 70, 16777215, CMD_4);
-	mlx_string_put(env->mlx, env->win, VP_WIDTH - 350, 90, 16777215, CMD_5);
-	mlx_string_put(env->mlx, env->win, VP_WIDTH - 350, 110, 16777215, CMD_6);
-	mlx_string_put(env->mlx, env->win, VP_WIDTH - 350, 130, 16777215, CMD_7);
+	mlx_string_put(env->mlx, env->win, VP_WIDTH - 350, 10, C_UI, CMD_1);
+	mlx_string_put(env->mlx, env->win, VP_WIDTH - 350, 30, C_UI, CMD_2);
+	mlx_string_put(env->mlx, env->win, VP_WIDTH - 350, 50, C_UI, CMD_3);
+	mlx_string_put(env->mlx, env->win, VP_WIDTH - 350, 70, C_UI, CMD_4);
+	mlx_string_put(env->mlx, env->win, VP_WIDTH - 350, 90, C_UI, CMD_5);
+	mlx_string_put(env->mlx, env->win, VP_WIDTH - 350, 110, C_UI, CMD_6);
+	mlx_string_put(env->mlx, env->win, VP_WIDTH - 350, 130, C_UI, CMD_7);
 }
 
 void				show_hud(t_env *env, int time_frame)
